4356836680c3e3a060528ec3ce44e7d8c96707cc
Luiza Leao <luiza.leao96@gmail.com>
2023-11-26 22:35:03 -0500

    Add POST route to /login. Set cookies.

--- express_server.js
+++ express_server.js
@@ -92,6 +92,13 @@ app.post("/urls/:id", (req, res) => {
   }
 });
 
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
+
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
